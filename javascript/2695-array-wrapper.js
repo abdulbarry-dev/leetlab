776cf9ddca6b6a2f5b2db0874a7c77b37b4f87df
abdulbarry-dev <abdulbarry.guenichi@gmail.com>
2025-11-02 20:18:58 +0100

    javascript/2695-array-wrapper.js

---
+++ javascript/2695-array-wrapper.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+class ArrayWrapper {
+  constructor(nums) {
+    this.nums = nums;
+  }
+}
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+  return this.nums.reduce((acc, num) => acc + num, 0);
+};
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function () {
+  return JSON.stringify(this.nums);
+};
+
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+console.log(obj1 + obj2); // 10
+console.log(String(obj1)); // "[1,2]"
+console.log(String(obj2)); // "[3,4]"
